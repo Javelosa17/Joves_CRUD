f820d43517bb75e4a911a544c5bc75e5da5b169d
Javelosa17 <144994679+Javelosa17@users.noreply.github.com>
2024-05-27 23:19:30 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBRfazcGb7jBo7f0Mj7nAF0edt2GBSReW0",
+  authDomain: "crud-joves.firebaseapp.com",
+  databaseURL: "https://crud-joves-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "crud-joves",
+  storageBucket: "crud-joves.appspot.com",
+  messagingSenderId: "650381848310",
+  appId: "1:650381848310:web:8a1e0d3bc303bc79f686d7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
